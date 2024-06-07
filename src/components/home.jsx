1dc020eb86742855d0384811ed09276b0ded9d82
KRISH KHANNA <krishkhanna00@gmail.com>
2024-06-07 10:54:12 +0530

    adding home page content

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,9 +2,59 @@ import React from "react";
 
 function Home() {
   return (
-    <>
-      <h1 className="text-gray-400">This is home</h1>
-    </>
+    <div className="text-[#cccccc] container mx-auto px-4 py-8">
+      <h1 className="text-[#ebd61e] font-bold">KAIZEN CART</h1>
+      <br />
+
+      <h2>Empowering Student Trade</h2>
+      <p>
+        KaizenCart is a student-developed website aiming to revolutionize trade
+        within your college. Recognizing the needs of students, KaizenCart
+        provides a platform to easily buy, sell, or exchange goods amongst
+        yourselves.
+      </p>
+      <br />
+
+      <h2>A Thriving Intra-College Marketplace</h2>
+      <p>
+        KaizenCart empowers students to become micro-entrepreneurs. Whether you
+        have textbooks, handcrafted art, or gently-used clothes, KaizenCart
+        offers a platform to connect with potential buyers. This fosters a
+        vibrant internal marketplace, promoting sustainable consumption and
+        eliminating the need for external vendors.
+      </p>
+      <br />
+
+      <h2>Made by Students, for Students</h2>
+      <p>
+        Developed by the Kaizen Technical Society, KaizenCart reflects a deep
+        understanding of student needs. Their technical expertise ensures a
+        user-friendly interface and a secure platform for transactions. This
+        student-centric approach fosters a sense of community ownership and
+        encourages continuous improvement.
+      </p>
+      <br />
+
+      <h2>Continuous Improvement and Scalability</h2>
+      <p>
+        KaizenCart holds immense potential for growth. Krish Khanna, the project
+        owner, is dedicated to continuous improvement based on user feedback.
+        The platform's core functionalities can be adapted to cater to a wider
+        audience, potentially evolving into a city or even nationwide student
+        marketplace.
+      </p>
+      <br />
+
+      <h2>In Conclusion</h2>
+      <p>
+        KaizenCart is more than just a website; it's a testament to student
+        ingenuity and a commitment to fostering a thriving intra-college
+        community. With its focus on ease of use, security, and continuous
+        improvement, KaizenCart holds immense promise for the future. We can
+        expect to see this student-driven initiative flourish and potentially
+        transform the way students trade within and beyond the college walls.
+      </p>
+    </div>
   );
 }
 
