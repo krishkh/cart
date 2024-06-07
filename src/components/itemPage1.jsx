133b4d437abd1a9d3b8ea5707c5798ec65a68398
KRISH KHANNA <krishkhanna00@gmail.com>
2024-06-07 17:13:16 +0530

    itemPage1 updated

--- src/components/itemPage1.jsx
+++ src/components/itemPage1.jsx
@@ -97,17 +97,17 @@ function ItemPage1({ data }) {
                 <p className="text-sm font-medium leading-none text-gray-500 dark:text-gray-400">
                   (5.0)
                 </p>
-                <a
+                <div
                   href="#"
                   className="text-sm font-medium leading-none text-gray-900 underline hover:no-underline dark:text-white"
                 >
                   345 Reviews
-                </a>
+                </div>
               </div>
             </div>
 
             <div className="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
-              <a
+              <div
                 href="#"
                 title=""
                 className="flex items-center justify-center py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
@@ -131,9 +131,9 @@ function ItemPage1({ data }) {
                   />
                 </svg>
                 Add to favorites
-              </a>
+              </div>
 
-              <a
+              <div
                 href="#"
                 title=""
                 className="text-white mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
@@ -157,7 +157,7 @@ function ItemPage1({ data }) {
                   />
                 </svg>
                 Add to cart
-              </a>
+              </div>
             </div>
 
             <hr className="my-6 md:my-8 border-gray-200 dark:border-gray-800" />
