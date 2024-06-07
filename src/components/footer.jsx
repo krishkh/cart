01850daeef329c880df6195c4e19ab9825919e08
KRISH KHANNA <krishkhanna00@gmail.com>
2024-06-07 16:27:56 +0530

    making footer non sticky

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 function Footer() {
   return (
-    <div className="flex flex-col mt-80">
-      <footer className="bg-gray-200 bottom-0 fixed w-full text-gray-100 py-4 px-4 border-[2px] border-solid border-white">
+    <div className="flex flex-col mt-60">
+      <footer className="bg-gray-200 bottom-0 w-full text-gray-100 py-4 px-4 border-[2px] border-solid border-white">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
           <p className="text-xs text-center">&copy; 2024 Your Company Name</p>
           <nav className="flex space-x-4">
--- src/components/itemSection.jsx
+++ src/components/itemSection.jsx
@@ -4,7 +4,7 @@ import ItemCard from "./itemCard";
 function ItemSection({ data }) {
   // console.log(data.data);
   return (
-    <div className="text-gray-400 flex-wrap bg-gray-200 body-font flex container px-5 py-5 mx-auto">
+    <div className="text-gray-400 mt-8 flex-wrap bg-gray-200 body-font flex container px-5 py-5 mx-auto">
       {data.data.map((item) => (
         <ItemCard key={item.itemNumber} data={item} /> // Use item.id for unique key
       ))}
