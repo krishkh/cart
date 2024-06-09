4e6deafeef169758cddc0dbf84dc9c7b8aa198b0
KRISH KHANNA <krishkhanna00@gmail.com>
2024-06-10 01:36:48 +0530

    fixed the popup opacity bug

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,17 +4,18 @@ function Modal({ children, isOpen, onClose }) {
   if (!isOpen) return null; // Don't render if not open
 
   return (
-    <div className="fixed top-[10.5rem] left-0 opacity-70 w-full bg-white dark:bg-gray-200  h-full z-10">
-      <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black text-white dark:bg-white dark:text-black rounded shadow  p-4">
+    <div className="fixed top-[10.5rem] left-0 w-full bg-[#ffffff55] dark:bg-gray-200  h-full z-10">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black text-white dark:bg-white dark:text-black rounded shadow">
         <button
           type="button"
           onClick={onClose}
-          className="mt-4 fixed top-0 right-0 text-red-500 hover:underline"
+          className="mt-4 fixed top-0 right- text-[40px] text-red-500"
         >
           x
         </button>
-        <div>{children}</div>
+        <div className="p-12">{children}</div>
       </div>
+      //
     </div>
   );
 }
