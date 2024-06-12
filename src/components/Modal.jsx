6aaa03bd10e3a0091e479e025d2fc667fe05c3a9
KRISH KHANNA <krishkhanna00@gmail.com>
2024-06-12 21:21:54 +0530

    fixed overlay issue with sign up modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,8 +12,8 @@ function Modal({ children, isOpen, onClose }) {
   };
 
   return (
-    <div className="fixed top-[10.5rem] left-0 w-full bg-[#ffffffcc] dark:bg-[#1b1b1bcc]  h-full z-10">
-      <div className="absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded shadow">
+    <div className="fixed top-0  left-0 w-full h-full bg-[#ffffffcc] dark:bg-[#1b1b1bcc] z-10">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded shadow">
         <button
           type="button"
           onClick={onClose}
